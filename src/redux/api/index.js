8b76107873f18f2c32cfe3805c10b59db60e9196
junaid <junaidali1507@gmail.com>
2021-07-26 12:10:56 +0530

    added files

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const url = "http://localhost:8050/api";
 
 export const getBlogs = () =>{
-    return axios.get(`${url}/blog`)
+    return axios.get(`${url}/blogs/blogs`)
 };
 export const addBlog = (formData) =>{
     return axios.post(`${url}/blog/add`, formData)
